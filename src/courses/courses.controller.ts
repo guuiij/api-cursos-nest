6bb8eac9bba068609fb12f5546ddb12bf6b9f2c5
guuiij <guilherme.jadson@hotmail.com>
2025-01-08 15:39:08 -0300

    docs: adicionada documentação da API com Swagger

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,29 +1,34 @@
 import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './create-course.dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('courses')
 export class CoursesController {
     constructor(private coursesService: CoursesService) { }
 
     @Get()
+    @ApiOkResponse({description: 'List of all Courses'})   // Criando coumentação de API 
     async getCourses() {
         const courses = await this.coursesService.getCourses();
         return courses;
     }
 
     @Get(':courseId')   //  Depois da nossa rota /courses , tera outra barro com ID -> localhost:3000/courses/2   
+    @ApiOkResponse({description: 'List of 1 Course'})  //
     async getCourse(@Param('courseId') courseId) {
         const courses = await this.coursesService.getCourse(courseId);
         return courses;
     }
     @Post()
+    @ApiCreatedResponse({description: 'Added a Course'})   // Aqui por ser POST ja usamos dessa forma
     async addCourse(@Body() createCourseDto: CreateCourseDto) {
         const courses = await this.coursesService.addCourse(createCourseDto);
         return courses;
     }
 
     @Delete()
+    @ApiOkResponse({description: 'Remove a Course'})  // Ao deletar ele retonar os outros curso entao é ApiOKResponse
     async deleteCourse(@Query() query ) {
         const courses = await this.coursesService.deleteCourse(query.courseId);
         return courses;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const options = new DocumentBuilder()
+    .setTitle("Courses API")
+    .setVersion("1.0.0")
+    .setDescription("API related to courses")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document)     // "api" é a porta que o document Swagger estara disponivel
+
+
+
+  await app.listen(3000);
+
+
 }
 bootstrap();
